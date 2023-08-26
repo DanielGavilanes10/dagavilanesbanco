f3f23413f48e0c937b4622098a1b17cdd3a6f42a
Daniel Gavilanes <dagavilanes2@espe.edu.ec>
2023-08-25 21:18:19 -0500

    Initial commit

---
+++ products-accounts/src/test/java/com/banquito/core/productsaccounts/ProductsAccountsApplicationTests.java
@@ -0,0 +1,10 @@
+package com.banquito.core.productsaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsAccountsApplicationTests {
+
+
+}
